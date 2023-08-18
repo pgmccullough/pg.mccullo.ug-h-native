5ac2855efd5da4ef176d912cc666e3fb7505418d
Patrick Glendon McCullough <p@mccullo.ug>
2023-08-18 12:14:55 -0400

    initial wysiwyg

--- components/Header/HeaderBar.tsx
+++ components/Header/HeaderBar.tsx
@@ -1,23 +1,61 @@
+import React, { useRef, useState } from 'react';
 import type { ReactNode } from 'react';
-
-const StyleSheet = require('react-native/Libraries/StyleSheet/StyleSheet');
-const Text = require('react-native/Libraries/Text/Text');
-const useColorScheme = require('react-native/Libraries/Utilities/useColorScheme');
-import { View } from 'react-native';
-import React from 'react';
+import {
+  KeyboardAvoidingView,
+  Platform,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View
+} from 'react-native';
+import {actions, RichEditor, RichToolbar} from "react-native-pell-rich-editor";
 
 export const HeaderBar = (): ReactNode => {
-  const isDarkMode = useColorScheme === 'dark';
+  const handleHead = ({tintColor}:any) => <Text style={{color: tintColor}}>H1</Text>
+  const richText = useRef<any>(null);
+  const [ active, setActive ] = useState<boolean>(false)
+  const [ newPostText, setNewPostText ] = useState<string>("")
+  const isDarkMode = useColorScheme() === 'dark';
   return (
+    <>
+    <View style={{position: 'absolute', zIndex: 2, top: 205, left: '21%', right: '2%'}} >
+      <SafeAreaView>
+        <ScrollView>
+          <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"}	style={{ flex: 1 }}>
+            <RichEditor
+              onFocus={() => setActive(true)}
+              onBlur={() => setActive(false)}
+              ref={richText}
+              onChange={(descriptionText:string) => {
+                setNewPostText(descriptionText);
+              }}
+            />
+          </KeyboardAvoidingView>
+        </ScrollView>
+
+        {active
+          ?<RichToolbar
+            editor={richText}
+            actions={[ actions.setBold, actions.setItalic, actions.setUnderline, actions.heading1 ]}
+            iconMap={{ [actions.heading1]: handleHead }}
+          />
+          :""
+        }
+      </SafeAreaView>
+      </View>
       <View
         style={styles.headerBar}
       >
         <Text
           style={styles.headerBarText}
         >
-          Patrick Glendon McCullough
+
+          {/* Patrick Glendon McCullough */}
         </Text>
       </View>
+    </>
   );
 };
 
